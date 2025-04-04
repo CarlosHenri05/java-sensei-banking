997f7aed50a78d8bd1c396c07b7af44968d8a24b
CarlosHenri05 <carloshenrique20051bb@gmail.com>
2025-04-04 01:14:26 -0300

    Adding models

---
+++ src/model/ContaCorrente.java
@@ -0,0 +1,49 @@
+package model;
+
+public class ContaCorrente extends Conta{
+
+  private String agencia;
+  private double limite;
+
+  public String getAgencia() {
+    return agencia;
+  }
+  public void setAgencia(String agencia) {
+    this.agencia = agencia;
+  }
+  public double getLimite() {
+    return limite;
+  }
+  public void setLimite(double limite) {
+    this.limite = limite;
+  }
+  public ContaCorrente(double saldo, int id, String agencia, double limite) {
+    super(saldo, id);
+    this.agencia = agencia;
+    this.limite = limite;
+  }
+
+  @Override
+  public void sacar(double valor) {
+    if (valor <= getSaldo() + limite) {
+      setSaldo(getSaldo() - valor);
+    } else {
+      System.out.println("Saldo insuficiente.");
+    }
+  }
+  @Override
+  public void depositar(double valor) {
+    setSaldo(getSaldo() + valor);
+  }
+
+  @Override
+  public void transferir(Conta contaDestino, double valor) {
+    if (valor <= getSaldo() + limite) {
+      sacar(valor);
+      contaDestino.depositar(valor);
+    } else {
+      System.out.println("Saldo insuficiente.");
+    }
+  }
+
+}
---
+++ src/model/ContaPoupanca.java
@@ -0,0 +1,41 @@
+package model;
+
+public class ContaPoupanca extends Conta {
+
+    private String agencia;
+
+    public String getAgencia() {
+        return agencia;
+    }
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+ 
+    public ContaPoupanca(double saldo, int id, String agencia) {
+        super(saldo, id);
+        this.agencia = agencia;
+    }
+
+    @Override
+    public void sacar(double valor) {
+        if (valor <= getSaldo()) {
+            setSaldo(getSaldo() - valor);
+        } else {
+            System.out.println("Saldo insuficiente.");
+        }
+    }
+    @Override
+    public void depositar(double valor) {
+        setSaldo(getSaldo() + valor);
+    }
+
+    public void transferir(Conta contaDestino, double valor) {
+      if (valor <= getSaldo()) {
+        sacar(valor);
+        contaDestino.depositar(valor);
+      } else {
+        System.out.println("Saldo insuficiente.");
+      }
+    }
+}
